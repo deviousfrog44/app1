57d9a2cf991620de37879a485d23857d68848f21
alho <dramalho07@proton.me>
2026-04-20 14:57:52 +0100

    added basic opengl initialization

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4.6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4.6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	printf("hola\n");
-	return 0;
+    GLFWwindow* window = glfwCreateWindow(800,800,"Hello World",NULL,NULL);
+    if (window==NULL) {printf("Failed to create GLFW window"); glfwTerminate(); return -1;}
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ printf("Failed to initialize GLAD"); return -1;}
+
+    glViewport(0,0,800,800);
+
+    while(!glfwWindowShouldClose(window)) {
+	    glfwSwapBuffers(window);
+	    glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
 }
